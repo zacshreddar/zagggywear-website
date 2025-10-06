b4752521a28c7c61b4a768aa831e0bb1acd01fef
zacshreddar <njorogeian48@gmail.com>
2025-10-06 15:11:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+  });
+});
